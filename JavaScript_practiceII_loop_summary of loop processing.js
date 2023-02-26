90ade34771f2ecd56a42c21abc670d3f176acd88
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-26 23:37:55 +0900

    Add files via upload

---
+++ JavaScript_practiceII_loop_summary of loop processing.js
@@ -0,0 +1,15 @@
+// Let's output 1 to 100 in order using the for statement.
+// If the number is a multiple of 3, print "It's a multiple of 3."
+
+// Please complete the for statement.
+for (let number=1;number<=100;number++) {
+  
+    // Use an if statement to print "It's a multiple of 3." if "number" is a multiple of 3, and print the number otherwise.
+    if(number % 3 === 0){
+      console.log("It's a multiple of 3.");
+    }else{
+      console.log(number);
+    }
+    
+  }
+  
